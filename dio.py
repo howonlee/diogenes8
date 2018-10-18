82e870b0c0c26f1e2bc0053e112b1c84db1b958d
Howon Lee <hleehowon@gmail.com>
2018-10-17 18:52:19 -0700

    progress

--- dio.py
+++ dio.py
@@ -10,29 +10,39 @@ def is_valid_dir(dir_to_check: str) -> bool:
     else:
         return os.path.isdir(dir_to_check)
 
-def get_people() -> Iterator[str]:
+def get_curr_dir() -> str:
+    return os.path.abspath(os.path.dirname(__file__))
+
+def get_people_list() -> Iterator[str]:
     """
     Gets the list of people corresponding to the current
     installation of diogenes
     """
-    curr_dir = os.path.abspath(os.path.dirname(__file__))
-    # log curr_dir
-    dirs = os.listdir(curr_dir)
+    dirs = os.listdir(get_curr_dir())
     return filter(is_valid_dir, dirs)
+
+def get_people() -> Iterator[Dict[str, Any]]:
+    return map(get_person, get_people_list())
 
 def get_person(person_dir: str) -> Dict[str, Any]:
     """
     Gets the dictionary representation of a person from
     a str denoting the directory corresponding to the person
     """
-    pass
+    peep_settings_path = os.path.join(person_dir, ".peep")
+    with open(peep_settings_path, "r") as peep_settings_file:
+        res = json.load(peep_settings_file)
+    return res
 
-def should_email_today(day: datetime.datetime) -> bool:
+def should_email_today(dt: datetime.datetime) -> bool:
     """
     Returns True if we should email ourselves today w/ reminders
     False otherwise
     """
-    pass
+    month = dt.month
+    if month % 3 == 0:
+        return True
+    return False
 
 def filter_people_for_day(
         people: Iterator[Dict[str, Any]],
@@ -41,13 +51,32 @@ def filter_people_for_day(
     Returns True if we should email ourselves today w/ reminders
     False otherwise
     """
+###########
+###########
+###########
+###########
     pass
 
-def email_self(people: Iterator[Dict[str, Any]], settings: Dict[str, Any]) -> None:
+def make_emails(people: Iterator[Dict[str, Any]]) -> Iterator[Dict[str, str]]:
+###########
+###########
+###########
+###########
+###########
+    pass
+
+def send_emails(settings: Dict[str, Any], emails: iterator[Dict[str, str]]) -> None:
+###########
+###########
+###########
+###########
     pass
 
 def get_settings() -> Dict[str, Any]:
-    pass
+    dio_settings_path = os.path.join(get_curr_dir(), ".dio")
+    with open(dio_settings_path, "r") as dio_settings_file:
+        res = json.load(dio_settings_file)
+    return res
 
 if __name__ == "__main__":
     print(list(get_people()))
