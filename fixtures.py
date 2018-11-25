a52c013d6370dcc746dd740e0adb1ca029dbc5e1
howon lee <hleehowon@gmail.com>
2018-11-24 19:30:51 -0800

    rely entirely on hypothesis

--- fixtures.py
+++
@@ -1,9 +0,0 @@
-import dio
-
-peep = dio.Person(email="bob@dobbs.com", name="Bob Dobbs")
-
-schedule = dio.DefaultSchedule()
-
-
-if __name__ == "__main__":
-    raise NotImplementedError("Import this module to import the fixtures")
