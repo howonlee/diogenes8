1ef9eb54b23cb7fc6d3f048772f65de93595754b
howon lee <hleehowon@gmail.com>
2018-11-25 05:19:56 -0800

    hyp fixtures in

--- test.py
+++ test.py
@@ -16,6 +16,14 @@ def person_str(draw, name=st.text(), email=st.text(), salt=st.text()):
 def dio_dir(draw, dirname=hy_fs.fspaths()):
     return dio.DioDir(dirname=draw(dirname))
 
+@st.composite
+def sched_str(draw):
+    return dio.DefaultSchedule()
+
+@st.composite
+def recs_str(draw):
+    return dio.Recs(people=st.lists(person_str()))
+
 @hp.given(peep=person_str())
 def test_person_init(peep):
     # assert not null
@@ -28,18 +36,21 @@ def test_person_file_encode_involution(fs, peep):
     ##############
     pass
 
+@hp.given(peep=person_str())
 def test_person_folder_encode_involution(peep):
     ##############
     ##############
     ##############
     pass
 
+@hp.given(peep=person_str())
 def test_is_peep_dir_idempotence(peep):
     ##############
     ##############
     ##############
     pass
 
+@hp.given(peep=person_str())
 def test_get_all_idempotence(peep):
     ##############
     ##############
@@ -55,18 +66,21 @@ def test_dio_dir_creation_idempotence(fs, dio_dir):
     dio_dir.create_if_not_exists()
     assert os.path.exists(dio_dir.dirname)
 
+@hp.given(recs=recs_str())
 def test_recs_encoding_involution(recs):
     ##############
     ##############
     ##############
     pass
 
+@hp.given(sched=sched_str())
 def test_schedule_should_email_day_idempotence(sched):
     ##############
     ##############
     ##############
     pass
 
+@hp.given(sched=sched_str())
 def test_schedule_should_contact_idempotence(sched):
     ##############
     ##############
@@ -79,12 +93,14 @@ def test_get_recs_idempotence():
     ##############
     pass
 
+@hp.given(peep=person_str())
 def test_add_person_idempotence(peep):
     ##############
     ##############
     ##############
     pass
 
+@hp.given(peep=person_str())
 def test_add_person_associativity(peep):
     ##############
     ##############
