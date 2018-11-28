be7ffcb802a0cb5229d99e4859134ae1dc215b3b
howon lee <hleehowon@gmail.com>
2018-11-28 13:52:43 -0800

    add settings to recommendd

--- recommendd.py
+++ recommendd.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3.7
 import dio
+import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 if __name__ == "__main__":
     sched = BlockingScheduler()
-    sched.add_job(dio.main_recs, 'interval', days=1)
+    first_run = datetime.datetime.now() + datetime.timedelta(seconds=10)
+    sched.add_job(
+            dio.main_recs,
+            trigger='interval',
+            days=1,
+            misfire_grace_time=9000,
+            coalesce=True,
+            max_instances=1,
+            next_run_time=first_run)
     sched.start()
