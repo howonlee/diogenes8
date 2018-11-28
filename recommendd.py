e8edbea8734882136e6bfa6a312f304795806ba1
howon lee <hleehowon@gmail.com>
2018-11-28 13:48:28 -0800

    shebang and stupid error in recommendd

--- recommendd.py
+++ recommendd.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3.7
 import dio
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 if __name__ == "__main__":
     sched = BlockingScheduler()
     sched.add_job(dio.main_recs, 'interval', days=1)
-    scheduler.start()
+    sched.start()
